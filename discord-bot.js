786e1140ef3ed13fb8cde32037962e0f5f92bc11
Tanner Shaw <traumaaa@gmail.com>
2023-10-01 17:31:45 -0500

    feature(addrooms): created /addrooms to add specific rooms the discord admin is in to their discord server

--- discord-bot.js
+++ discord-bot.js
@@ -1,13 +1,14 @@
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits, ActionRowBuilder, StringSelectMenuBuilder } from 'discord.js';
+
 import axios from 'axios';
 import 'dotenv/config'
 
 // TODO Move this into the Discreetly database and let it be queried with an API key/Password
 export const roleMap = {
-  '1123434145423560794': {
+  '1063141052187426856': {
     'alpha-tester': '1406889119610943773982914340053908893373464304417165775622512450080102390258'
   },
-  '943612659163602974': {
+  '1063141052187426856': {
     'explorer': '16126092212458677464797669730808312928970541841197462821829418244240512408136'
   }
 };
@@ -21,26 +22,82 @@ const TOKEN = process.env.DISCORDTOKEN
 
 client.once('ready', () => {
   console.log('Bot is ready!');
-});
+  client.guilds.cache.forEach(guild => {
 
-client.on('messageCreate', async (message) => {
-  if (message.content === '!requestcode') {
-    const discordServer = message.guild.id;
+    guild.commands.create({
+      name: 'requestcode',
+      description: 'Receive invite code for discreetly rooms'
+    }
+    )
+    .then(command => console.log(`Created command ${command.name}`))
+    .catch(console.error);
 
-    if (!roleMap[discordServer]) {
-      return message.reply('This server is not configured.');
+    guild.commands.create({
+      name: 'addrooms',
+      description: 'Add Discreetly Rooms to your discord server'
+    })
+    .then(command => console.log(`Created command ${command.name}`))
+    .catch(console.error);
+
+    guild.commands.create({
+      name: 'help',
+      description: 'Get help with Discreetly Bot commands'
+    })
+    .then(command => console.log(`Created command ${command.name}`))
+    .catch(console.error);
+  });
+
+
+  client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'help') {
+      await interaction.reply({ content: 'Admins can select rooms to to your server with ***/addrooms*** and users can request a code with ***/requestcode***', ephemeral: true });
     }
 
-    const member = message.guild.members.cache.get(message.author.id);
+    if (commandName === 'addrooms') {
+      const foundRooms = await axios.post(`${process.env.SERVERURL}/api/discord/rooms`, {
+        discordUserId: interaction.user.id
+      }, {
+        headers: {
+          'Authorization': `Basic ${Buffer.from(`${process.env.USERNAME}:${process.env.PASSWORD}`).toString('base64')}`,
+          'Content-Type': 'application/json'
+        }
+      }
+     );
 
-    if (PROVIDED_CODES.has(message.author.id)) {
-      return message.reply('You have already received a code!');
+     const roomChoices = foundRooms.data.map(room => {
+        return {
+          label: room.name,
+          value: room.roomId
+        }
+     })
+
+      const select = new StringSelectMenuBuilder()
+      .setCustomId('rooms')
+      .setPlaceholder('Select Rooms')
+      .setMinValues(1)
+      .setMaxValues(roomChoices.length)
+      .addOptions(roomChoices)
+
+      const rows = new ActionRowBuilder()
+      .addComponents(select)
+
+      await interaction.reply({ content: 'Select the rooms you want to add to your server', components: [rows], ephemeral: true });
     }
+    if (commandName === 'requestcode') {
+      const discordServer = interaction.guildId;
 
+
+    const member = interaction.user.id;
+    const interactionMember = interaction.member;
+    const roles = interactionMember.roles.cache.map(role => role.name);
 
     // Find the role the member has that matches a key in roleMap
     const roleRoomIds = Object.keys(roleMap[discordServer])
-      .filter(roleName => member?.roles.cache.some(role => role.name === roleName))
+      .filter(roleName => interactionMember.roles.cache.some(role => role.name === roleName))
       .map(roleName => roleMap[discordServer][roleName]);
 
 
@@ -56,16 +113,16 @@ client.on('messageCreate', async (message) => {
         }
       });
 
-      if (!discordIds.data.includes(message.author.id)) {
+      if (!discordIds.data.includes(member)) {
         roomIdArr.push(roomId);
         console.log(roomIdArr);
       }
     }));
 
-    console.log(roomIdArr.length);
+
 
     if (roomIdArr.length === 0) {
-      return message.reply('You have already received a code!');
+      return await interaction.reply("You have already received a code or don't have a proper role");
     }
 
     // Find the role the member has that matches a key in roleMap
@@ -83,14 +140,12 @@ client.on('messageCreate', async (message) => {
           'Content-Type': 'application/json'
         }
       })
-        .then(response => {
+        .then(async (response) => {
           let code = response.data.codes[0].claimcode
-          message.reply({ content: `Here's your code: ${process.env.CLIENTURL}/join/${code}`, ephemeral: true })
-          message.author.send(`Here's your code: ${process.env.CLIENTURL}/join/${code}`);
-          PROVIDED_CODES.add(message.author.id);
+          await interaction.reply({ content: `Here's your code: ${process.env.CLIENTURL}/join/${code}`, ephemeral: true })
           roleRoomIds.forEach(roomId => {
             axios.post(`${process.env.SERVERURL}/api/discord/add`, {
-              discordUserId: message.author.id,
+              discordUserId: member,
               roomId: roomId,
             }, {
               headers: {
@@ -107,7 +162,10 @@ client.on('messageCreate', async (message) => {
     } else {
       message.reply('You do not have the required role.');
     }
-  }
+
+    }
+  });
 });
+
 
 client.login(TOKEN);
